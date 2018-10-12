014939e3f19e51c751a62c125c024c2346424e8a
nedvetskayaa <43704378+nedvetskayaa@users.noreply.github.com>
2018-10-13 00:45:45 +0300

    Lesson3 Task1

---
+++ Task1.java
@@ -0,0 +1,9 @@
+public class Task1 {
+
+    public static void main(String[] args) {
+        int h=2, d=4, u=5;
+        System.out.print("Hundreds: " + h + ".");
+        System.out.print(" Dozens: " + d + ".");
+        System.out.print(" Units: " + u + ".");
+    }
+}
