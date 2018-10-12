926afd48b4fcff4b341292629cb072f1fee7c436
nedvetskayaa <43704378+nedvetskayaa@users.noreply.github.com>
2018-10-13 00:47:22 +0300

    Lesson3 Task2,3,5

---
+++ Task2.java
@@ -0,0 +1,19 @@
+public class Task2 {
+
+    public static void main(String[] args) {
+        float C = 14.7f ;
+        float K = 273.15f, sumK;
+        float F = 1.8f, sumF;
+
+        System.out.println("Celsius: " + C);
+
+        sumK= C+K;
+        System.out.println("Kelvin: " + sumK);
+
+        sumF= C*F;
+        System.out.print("Fahrenheit: " + sumF + 32);
+
+
+
+    }
+}
