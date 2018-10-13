ae3bd9ecbde42393ff83ba4eb34490eba76fe8b2
nedvetskayaa <43704378+nedvetskayaa@users.noreply.github.com>
2018-10-13 22:34:34 +0300

    Delete Task1.java

--- Task1.java
+++
@@ -1,9 +0,0 @@
-public class Task1 {
-
-    public static void main(String[] args) {
-        int h=2, d=4, u=5;
-        System.out.print("Hundreds: " + h + ".");
-        System.out.print(" Dozens: " + d + ".");
-        System.out.print(" Units: " + u + ".");
-    }
-}
