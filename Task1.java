8508d7bb5d0c0efa1b69f4c2a907bd9c97bf08fd
nedvetskayaa <43704378+nedvetskayaa@users.noreply.github.com>
2018-10-18 01:55:42 +0300

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,12 @@
+public class Task1 {
+
+    public static void main(String[] args) {
+        int position = 8;
+        if (position < 5) {
+            System.out.print("Вы в топ-5 " + position);
+        } else {
+            System.out.print("Вы не попали в топ-5: " + position);
+        }
+
+    }
+}
---
+++ Task2.java
@@ -0,0 +1,16 @@
+public class Task2 {
+
+    public static void main(String[] args) {
+        int countKillTanksFirstGamer = 12;
+        int countKillTanksSecondGamer = 8;
+        int scoreFirstGamer = countKillTanksFirstGamer * 100; //1200
+        int scoreSecondGamer = countKillTanksSecondGamer * 100; //800
+        int getExtraPoint = scoreFirstGamer + 1000;
+        if (countKillTanksFirstGamer > countKillTanksSecondGamer) {
+            System.out.println("First Gamer: " + scoreFirstGamer);
+            System.out.println("First Gamer get extra point: " + getExtraPoint);
+            System.out.println("Second Gamer: " + scoreSecondGamer);
+
+        }
+    }
+}
